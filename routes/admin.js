7835cf536f802b956d1a38ccedef1c14c27de89e
kiddos <kiddo831007@gmail.com>
2017-11-06 14:55:34 +0800

    add login message

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var admins = require('../models/admins');
+var colors = require('colors');
 
 router.get('/', function(req, res) { res.render('admin', {title: 'Login'}); });
 
@@ -8,6 +9,7 @@ router.post('/', function(req, res) {
   var admin = new admins.Admin(req.body.username, req.body.password);
   admin.login(function(status) {
     if (status) {
+      console.log(colors.green(username + ' login success.'));
       req.session.login = true;
       req.session.save(function(err) {
         if (err) {
@@ -17,6 +19,7 @@ router.post('/', function(req, res) {
         }
       });
     } else {
+      console.log(color.red(username + ' login failed.'));
       res.redirect('/login');
     }
   });
