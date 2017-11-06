f3131cc6231f44b64517daa50e46fdc4a31f6110
kiddos <kiddo831007@gmail.com>
2017-11-06 13:35:09 +0800

    add secret key for app

--- settings.js
+++ settings.js
@@ -1,5 +1,8 @@
 var path = require('path');
 
-var settings = {db: path.join(__dirname, 'db.sqlite3')};
+var settings = {
+  db: path.join(__dirname, 'db.sqlite3'),
+  secret: 'OTljNjgzNTU1NDM0NjY2MjM5ZDdmMDRj'
+};
 
 module.exports = settings;
