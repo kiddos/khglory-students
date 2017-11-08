6362ee5ad0ba2d529f7add0d5a7b8ba2f197352f
kiddos <kiddo831007@gmail.com>
2017-11-08 12:22:44 +0800

    set title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.session.login) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: '管理'});
   } else {
     res.redirect('/login');
   }
