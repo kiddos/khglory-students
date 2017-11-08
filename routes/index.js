d2dbf399c0c394aefbae87da62c404687b9241ef
kiddos <kiddo831007@gmail.com>
2017-11-08 14:28:18 +0800

    add logout button

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.session.login) {
-    res.render('index', {title: '管理'});
+    res.render('index', {title: '管理', login: true});
   } else {
     res.redirect('/login');
   }
