75bd34de26570225b0de75c759120d31b26e0f2f
kiddos <kiddo831007@gmail.com>
2017-11-08 19:40:51 +0800

    complete migration

--- migration.js
+++ migration.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
-var admins = require('./models/admins.js');
-var students = require('./models/students.js');
+var admins = require('./models/admins');
+var students = require('./models/students');
+var teachers = require('./models/teachers');
+var classes = require('./models/classes');
 
 admins.migrate();
 students.migrate();
+teachers.migrate();
+classes.migrate();
