65aeb25d8b56e8952809ab204263e39faf7b4b6d
kiddos <kiddo831007@gmail.com>
2017-11-09 12:54:15 +0800

    route add page

--- routes/students.js
+++ routes/students.js
@@ -14,4 +14,65 @@ router.get('/', function(req, res, next) {
   }
 });
 
+router.get('/add', function(req, res, next) {
+  if (req.session.login) {
+    students.queryAll(function(allStudents) {
+      res.render('student_add', {
+        title: '學生管理',
+        login: true,
+        students: allStudents,
+        fields: [
+          {
+            name: 'id',
+            text: '號碼',
+            require: true,
+          },
+          {
+            name: 'name',
+            text: '姓名',
+            require: true,
+          },
+          {
+            name: 'gender',
+            text: '性別',
+            require: false,
+          },
+          {
+            name: 'birthday',
+            text: '生日',
+            require: false,
+          },
+          {
+            name: 'socialId',
+            text: '身份證字號',
+            require: false,
+          },
+          {
+            name: 'marriage',
+            text: '婚姻',
+            require: false,
+          },
+          {
+            name: 'address',
+            text: '地址',
+            require: false,
+          },
+          {
+            name: 'phone',
+            text: '電話',
+            require: false,
+          },
+          {
+            name: 'email',
+            text: '電子郵件',
+            require: false,
+          },
+        ]
+      });
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
+
 module.exports = router;
