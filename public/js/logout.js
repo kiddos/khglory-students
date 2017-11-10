9af6e29dc63293a136fa316502d2da98a9f85c94
kiddos <kiddo831007@gmail.com>
2017-11-10 14:03:33 +0800

    add on click event at document ready

--- public/js/logout.js
+++ public/js/logout.js
@@ -2,18 +2,18 @@ $(document).ready(function() {
   $('#logout').on('click', function() {
     $('#popup-title').text('登出');
     $('#popup-text').text('您確定要登出嗎？');
-    $('#popup-dialog').fadeIn(function() {
-      $('#yes').on('click', function() {
-        $.post('/logout').done(function(msg) {
-          window.location.href = '/';
-        }).fail(function(xhr, status, err) {
-          window.location.href = '/';
-        });
-      });
+    $('#popup-dialog').fadeIn();
+  });
 
-      $('#no').on('click', function() {
-        $('#popup-dialog').fadeOut();
-      });
+  $('#yes').on('click', function() {
+    $.post('/logout').done(function(msg) {
+      window.location.href = '/';
+    }).fail(function(xhr, status, err) {
+      window.location.href = '/';
     });
+  });
+
+  $('#no').on('click', function() {
+    $('#popup-dialog').fadeOut();
   });
 });
