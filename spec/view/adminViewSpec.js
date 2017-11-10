03dd0c47d7ebc2b86c11ebcea2b57ba5e8f6a2c4
kiddos <kiddo831007@gmail.com>
2017-11-10 14:03:11 +0800

    fix logout

--- spec/view/adminViewSpec.js
+++ spec/view/adminViewSpec.js
@@ -1,5 +1,4 @@
 var webdriver = require('selenium-webdriver');
-// var chrome = require('selenium-webdriver/chrome');
 var By = webdriver.By;
 var until = webdriver.until;
 var Key = webdriver.Key;
@@ -7,51 +6,45 @@ var Key = webdriver.Key;
 var driver = new webdriver.Builder().forBrowser('chrome').build();
 
 describe('Admin Login Page', function() {
-
-  it('Should be able to login with correct username and password', function(
-                                                                       done) {
+  it('Should be able to login with correct account', function(done) {
     driver.get('http://localhost:3000');
     var usernameElement = driver.findElement(By.css('label[for="username"]'));
     usernameElement.getText().then(function(text) {
       expect(text).toBe('使用者');
+    });
 
-      var passwordElement =
-          driver.findElement(By.css('label[for="password"]'));
-      passwordElement.getText().then(function(text) {
-        expect(text).toBe('密碼');
-        var userField = driver.findElement(By.css('input[name="username"]'));
-        userField.sendKeys('admin1');
-        var passField = driver.findElement(By.css('input[name="password"]'));
-        passField.sendKeys('admin1');
-        var submit = driver.findElement(By.css('input[name="submit"]'));
-        submit.sendKeys(Key.RETURN);
+    var passwordElement = driver.findElement(By.css('label[for="password"]'));
+    passwordElement.getText().then(function(text) {
+      expect(text).toBe('密碼');
+    });
 
-        driver.wait(function() {
-          return driver.getTitle().then(function(title) {
-            expect(title).toBe('管理');
-            done();
-          });
-        }, 6000);
+    var userField =
+    driver.findElement(By.css('input[name="username"]'));
+    userField.sendKeys('admin1');
+    var passField =
+    driver.findElement(By.css('input[name="password"]'));
+    passField.sendKeys('admin1');
+    var submit = driver.findElement(By.css('input[name="submit"]'));
+    submit.sendKeys(Key.RETURN);
+
+    driver.wait(function() {
+      return driver.getTitle().then(function(title) {
+        expect(title).toBe('管理');
+        done();
       });
-    }, 10000);
-  });
+    }, 3000);
+  }, 10000);
 
   afterEach(function(done) {
-    setTimeout(function() {
-      var logoutButton = driver.findElement(By.css('#logout'));
-      logoutButton.click();
+    var logoutButton = driver.findElement(By.id('logout'));
+    logoutButton.click();
 
-      setTimeout(function() {
-        var yesButton = driver.findElement(By.id('yes'));
-        yesButton.click();
-
-        driver.wait(function() {
-          return driver.getTitle().then(function(title) {
-            expect(title).toBe('管理');
-            done();
-          });
-        });
-      }, 2000);
-    }, 1000);
+    var yesButton = driver.findElement(By.id('yes'));
+    driver.wait(until.elementIsVisible(yesButton), 6000).then(function() {
+      yesButton.click();
+      driver.wait(until.titleIs('Login'), 6000).then(function() {
+        done();
+      });
+    });
   }, 10000);
 });
