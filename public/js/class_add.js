9564345d2be9bec31492ee46db48786e3dcb028c
kiddos <kiddo831007@gmail.com>
2017-11-16 06:18:51 +0800

    changing view

--- public/js/class_add.js
+++ public/js/class_add.js
@@ -21,13 +21,25 @@ $(document).ready(function() {
     }
   }
 
+  function changeView() {
+    for (var p = 0; p < 3 ; ++p) {
+      if (p === phase) {
+        $('#phase' + (p + 1)).removeClass('hide');
+      } else {
+        $('#phase' + (p + 1)).addClass('hide');
+      }
+    }
+  }
+
   $('#previous').on('click', function() {
     if (phase > 0) phase -= 1;
     changeButton();
+    changeView();
   });
 
   $('#next').on('click', function() {
     if (phase < 2) phase += 1;
     changeButton();
+    changeView();
   });
 });
