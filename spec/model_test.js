cfcfa179985ca4918b0bb7e79272fe2c3220d7db
kiddos <kiddo831007@gmail.com>
2017-11-23 12:40:58 +0800

    run migration before testing

--- spec/model_test.js
+++ spec/model_test.js
@@ -1,6 +1,16 @@
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
 
+var admins = require('../models/admins');
+var students = require('../models/students');
+var teachers = require('../models/teachers');
+var classes = require('../models/classes');
+
+admins.migrate();
+students.migrate();
+teachers.migrate();
+classes.migrate();
+
 jasmine.loadConfig({
   "spec_dir": "spec",
   "spec_files": ["./models/*spec.js"],
