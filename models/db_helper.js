fb0b402c9771d7207986f24fd5cce38d952db74b
kiddos <kiddo831007@gmail.com>
2017-11-23 12:38:57 +0800

    search for environment variable debug for debug mode

--- models/db_helper.js
+++ models/db_helper.js
@@ -3,13 +3,22 @@ var settings = require('../settings.js');
 var colors = require('colors');
 var TransactionDatabase = require("sqlite3-transactions").TransactionDatabase;
 
-
-var db =
-    new TransactionDatabase(new sqlite3.Database(settings.db, function(err) {
-      if (err) {
-        return console.error(colors.red(err.messgage));
-      }
-      console.log('Connected to sqlite3 database');
-    }));
+var db;
+if (process.env.DEBUG === 'TRUE') {
+  db = new TransactionDatabase(new sqlite3.Database(':memory:', function(err) {
+    if (err) {
+      return console.error(colors.red(err.messgage));
+    }
+    console.log('Connected to sqlite3 memory');
+  }));
+} else {
+  db =
+      new TransactionDatabase(new sqlite3.Database(settings.db, function(err) {
+        if (err) {
+          return console.error(colors.red(err.messgage));
+        }
+        console.log('Connected to sqlite3 database');
+      }));
+}
 
 module.exports = db;
