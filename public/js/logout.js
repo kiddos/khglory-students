37440bfc41e7f826ec37b073e7c92c077b2d6060
kiddos <kiddo831007@gmail.com>
2017-11-24 17:36:45 +0800

    use popup dialog

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,19 +1,11 @@
 $(document).ready(function() {
   $('#logout').on('click', function() {
-    $('#popup-title').text('登出');
-    $('#popup-text').text('您確定要登出嗎？');
-    $('#popup-dialog').fadeIn();
-  });
-
-  $('#yes').on('click', function() {
-    $.post('/logout').done(function(msg) {
-      window.location.href = '/';
-    }).fail(function(xhr, status, err) {
-      window.location.href = '/';
+    popupMessage('登出', '您確定要登出嗎？', function() {
+      $.post('/logout').done(function(msg) {
+        window.location.href = '/';
+      }).fail(function(xhr, status, err) {
+        window.location.href = '/';
+      });
     });
-  });
-
-  $('#no').on('click', function() {
-    $('#popup-dialog').fadeOut();
   });
 });
