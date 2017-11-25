eff15b0adb06bb15d60b0cb2ba24f432fc4ddb6a
kiddos <kiddo831007@gmail.com>
2017-11-25 10:15:39 +0800

    render user

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.session.login) {
-    res.render('index', {title: '管理', login: true});
+    res.render('index', {
+      title: '管理',
+      login: true,
+      user: req.session.user,
+    });
   } else {
     res.redirect('/login');
   }
