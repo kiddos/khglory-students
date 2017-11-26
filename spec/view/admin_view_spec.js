28da82d54ea9d1de645067bfbb23eb013891ee26
kiddos <kiddo831007@gmail.com>
2017-11-26 23:21:35 +0800

    test with incorrect usename and password

--- spec/view/admin_view_spec.js
+++ spec/view/admin_view_spec.js
@@ -1,13 +1,22 @@
+
 var webdriver = require('selenium-webdriver');
 var By = webdriver.By;
 var until = webdriver.until;
 var Key = webdriver.Key;
 
-var driver = new webdriver.Builder().forBrowser('chrome').build();
-
 describe('Admin Login Page', function() {
+  'use strict';
+
+  var driver;
+
+  beforeEach(function(done) {
+    driver = new webdriver.Builder().forBrowser('chrome').build();
+    driver.get('http://localhost:3000').then(function() {
+      driver.wait(until.titleIs('Login')).then(function() { done(); });
+    });
+  });
+
   it('Should be able to login with correct account', function(done) {
-    driver.get('http://localhost:3000');
     var usernameElement = driver.findElement(By.css('label[for="username"]'));
     usernameElement.getText().then(function(text) {
       expect(text).toBe('使用者');
@@ -23,27 +32,39 @@ describe('Admin Login Page', function() {
     var passField = driver.findElement(By.css('input[name="password"]'));
     passField.sendKeys('admin1');
     var submit = driver.findElement(By.css('input[name="submit"]'));
-    submit.sendKeys(Key.RETURN);
+    submit.sendKeys(Key.RETURN).then(function() {
+      driver.wait(until.titleIs('高雄榮光堂統計系統'), 3000).then(function() {
+        var logoutButton = driver.findElement(By.id('logout'));
+        logoutButton.click();
 
-    driver.wait(function() {
-      return driver.getTitle().then(function(title) {
-        expect(title).toBe('管理');
-        done();
-      });
-    }, 3000);
-  }, 10000);
-
-  afterEach(function(done) {
-    var logoutButton = driver.findElement(By.id('logout'));
-    logoutButton.click();
-
-    var yesButton = driver.findElement(By.id('yes'));
-    driver.wait(until.elementIsVisible(yesButton), 6000).then(function() {
-      yesButton.click();
-      driver.wait(until.titleIs('Login'), 6000).then(function() {
-        driver.close();
-        done();
+        var yesButton = driver.findElement(By.id('yes'));
+        driver.wait(until.elementIsVisible(yesButton), 6000).then(function() {
+          yesButton.click();
+          done();
+        });
       });
     });
   }, 10000);
+
+  it('Should not be able to login with incorrect usename/password',
+     function(done) {
+    var userField = driver.findElement(By.css('input[name="username"]'));
+    userField.sendKeys('random username');
+    var passField = driver.findElement(By.css('input[name="password"]'));
+    passField.sendKeys('random password');
+    var submit = driver.findElement(By.css('input[name="submit"]'));
+
+    submit.sendKeys(Key.RETURN).then(function() {
+      driver.getTitle().then(function(title) {
+        expect(title).toBe('Login');
+        done();
+      });
+    });
+  });
+
+  afterEach(function(done) {
+    driver.wait(until.titleIs('Login'), 6000).then(function() {
+      driver.close().then(done);
+    });
+  });
 });
