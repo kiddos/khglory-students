bd6fc7f61b63d4cda1168565125fb25454dd1252
kiddos <kiddo831007@gmail.com>
2017-11-30 14:20:47 +0800

    change username and password

---
+++ public/js/admin.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('#submit').on('click', function() {
+    if ($('#re-password').val() === $('#password').val()) {
+      $.ajax({
+        url: '/admin',
+        method: 'POST',
+        data: {
+          username: $('#username').val(),
+          password: $('#password').val(),
+        }
+      }).done(function(status) {
+        if (status === 'success') {
+          $('#admin a').text($('#username').val());
+          infoMessage('更改成功', 600);
+        } else {
+          errorMessage('更改失敗', 600);
+        }
+      });
+    }
+  });
+});
+
+$(window).on('keypress', function(event) {
+  if (event.key === 'Enter') {
+    $('#submit').click();
+  }
+});
