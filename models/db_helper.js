193e810acd2efc65a088795e02ed91de62384d97
kiddos <kiddo831007@gmail.com>
2017-11-30 10:15:29 +0800

    enable foriegn key

--- models/db_helper.js
+++ models/db_helper.js
@@ -3,21 +3,35 @@ var settings = require('../settings.js');
 var colors = require('colors');
 var TransactionDatabase = require("sqlite3-transactions").TransactionDatabase;
 
+function enableForiegnKey(db) {
+  db.run('PRAGMA foreign_keys = ON;', function(err) {
+    if (err) {
+      console.error(colors.red(err.messgage));
+    } else {
+      console.log(colors.green('enable foriegn keys'));
+    }
+  });
+}
+
 var db;
 if (process.env.DEBUG === 'TRUE') {
   db = new TransactionDatabase(new sqlite3.Database(':memory:', function(err) {
     if (err) {
-      return console.error(colors.red(err.messgage));
+      console.error(colors.red(err.messgage));
+    } else {
+      console.log('Connected to sqlite3 memory');
+      enableForiegnKey(db);
     }
-    console.log('Connected to sqlite3 memory');
   }));
 } else {
   db =
       new TransactionDatabase(new sqlite3.Database(settings.db, function(err) {
         if (err) {
-          return console.error(colors.red(err.messgage));
+          console.error(colors.red(err.messgage));
+        } else {
+          console.log('Connected to sqlite3 database');
+          enableForiegnKey(db);
         }
-        console.log('Connected to sqlite3 database');
       }));
 }
 
