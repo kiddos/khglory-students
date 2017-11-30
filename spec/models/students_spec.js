d71ff583b66812f71b6e9f4ff898d02fe719a6d4
kiddos <kiddo831007@gmail.com>
2017-11-30 11:17:05 +0800

    more test for extra info

--- spec/models/students_spec.js
+++ spec/models/students_spec.js
@@ -363,11 +363,21 @@ describe('Student BasicInfo', function() {
 });
 
 describe('Student ExtraInfo', function() {
-  it('Should be able to add ExtraInfo', function(done) {
+  beforeEach(function(done) {
     var name = '中文' + faker.name.firstName() + ' ' + faker.name.lastName();
     var student = new students.Student(faker.random.uuid(), name);
+
     student.insert(function(status) {
       expect(status).toBe(true);
+      done();
+    });
+  });
+
+  it('Should be able to be added', function(done) {
+    students.queryAll(function(allStudents) {
+      var student =
+          new students.Student(allStudents[0].id, allStudents[0].name);
+
       var extraInfo = new students.ExtraInfo({
         studentId: student.id,
         career: faker.name.jobTitle(),
@@ -390,11 +400,69 @@ describe('Student ExtraInfo', function() {
     });
   });
 
-  it('Should be able to update its ExtraInfo', function(done) {
-    var name = '中文' + faker.name.firstName() + ' ' + faker.name.lastName();
-    var student = new students.Student(faker.random.uuid(), name);
-    student.insert(function(status) {
-      expect(status).toBe(true);
+  it('Should not be able to be added with incorrect id', function(done) {
+    students.queryAll(function(allStudents) {
+      var student =
+          new students.Student(allStudents[0].id, allStudents[0].name);
+
+      var extraInfo = new students.ExtraInfo({
+        studentId: faker.random.uuid(),
+        career: faker.name.jobTitle(),
+        education: faker.random.boolean() ? '大專畢業' : '碩博士',
+        religion: faker.random.boolean() ? '基督教' : '佛教',
+        illness: faker.random.boolean() ? '無' : '生病',
+        emergencyContact: faker.random.boolean() ? '父' : '母',
+        emergencyContactPhone: faker.phone.phoneNumberFormat(1),
+      });
+      student.addExtraInfo(extraInfo, function(status) {
+        expect(status).toBe(false);
+        done();
+      });
+    });
+  });
+
+  it('Should be able to be added with partial info', function(done) {
+    students.queryAll(function(allStudents) {
+      var student =
+          new students.Student(allStudents[0].id, allStudents[0].name);
+
+      var extraInfo = new students.ExtraInfo({
+        studentId: student.id,
+        career: faker.name.jobTitle(),
+        education: faker.random.boolean() ? '大專畢業' : '碩博士',
+        religion: faker.random.boolean() ? '基督教' : '佛教',
+        illness: faker.random.boolean() ? '無' : '生病',
+        emergencyContact: faker.random.boolean() ? '父' : '母',
+        emergencyContactPhone: faker.phone.phoneNumberFormat(1),
+      });
+      var keys = Object.keys(extraInfo);
+      for (var i = 0; i < keys.length; ++i) {
+        var key = keys[i];
+        if (key !== 'studentId') {
+          if (faker.random.boolean()) {
+            extraInfo[key] = null;
+          }
+        }
+      }
+      student.addExtraInfo(extraInfo, function(status) {
+        expect(status).toBe(true);
+        student.getExtraInfo(function(extraInfoData) {
+          var keys = Object.keys(extraInfoData);
+          for (var i = 0; i < keys.length; ++i) {
+            var key = keys[i];
+            expect(extraInfo[key]).toBe(extraInfoData[key]);
+          }
+          done();
+        });
+      });
+    });
+  });
+
+  it('Should be able to be updated', function(done) {
+    students.queryAll(function(allStudents) {
+      var student =
+          new students.Student(allStudents[0].id, allStudents[0].name);
+
       var extraInfo = new students.ExtraInfo({
         studentId: student.id,
         career: faker.name.jobTitle(),
@@ -423,6 +491,71 @@ describe('Student ExtraInfo', function() {
               expect(info[key]).toBe(
                   newExtraInfo[key] === undefined ? null : newExtraInfo[key]);
             }
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  it('Should be not be able to be updated with incorrect id', function(done) {
+    students.queryAll(function(allStudents) {
+      var student =
+          new students.Student(allStudents[0].id, allStudents[0].name);
+
+      var extraInfo = new students.ExtraInfo({
+        studentId: student.id,
+        career: faker.name.jobTitle(),
+        education: faker.random.boolean() ? '大專畢業' : '碩博士',
+        religion: faker.random.boolean() ? '基督教' : '佛教',
+        illness: faker.random.boolean() ? '無' : '生病',
+        emergencyContact: faker.random.boolean() ? '父' : '母',
+        emergencyContactPhone: faker.phone.phoneNumberFormat(1),
+      });
+      student.addExtraInfo(extraInfo, function(status) {
+        expect(status).toBe(true);
+        var newExtraInfo = new students.ExtraInfo({
+          studentId: faker.random.uuid(),
+          career: faker.name.jobTitle(),
+          education: faker.random.boolean() ? '大專畢業' : '碩博士',
+          religion: faker.random.boolean() ? '基督教' : '佛教',
+          illness: faker.random.boolean() ? '無' : '生病',
+          emergencyContact: faker.random.boolean() ? '父' : '母',
+          emergencyContactPhone: faker.phone.phoneNumberFormat(1),
+        });
+        student.updateExtraInfo(newExtraInfo, function(status) {
+          expect(status).toBe(false);
+          done();
+        });
+      });
+    });
+  });
+
+  it('Should be deleted when the student is removed', function(done) {
+    students.queryAll(function(allStudents) {
+      var student =
+          new students.Student(allStudents[0].id, allStudents[0].name);
+
+      var extraInfo = new students.ExtraInfo({
+        studentId: student.id,
+        career: faker.name.jobTitle(),
+        education: faker.random.boolean() ? '大專畢業' : '碩博士',
+        religion: faker.random.boolean() ? '基督教' : '佛教',
+        illness: faker.random.boolean() ? '無' : '生病',
+        emergencyContact: faker.random.boolean() ? '父' : '母',
+        emergencyContactPhone: faker.phone.phoneNumberFormat(1),
+      });
+      student.addExtraInfo(extraInfo, function(status) {
+        expect(status).toBe(true);
+
+        student.remove(function(status) {
+          expect(status).toBe(true);
+          students.getExtraInfo(function(extraInfos) {
+            var found = false;
+            for (var i = 0; i < extraInfos.length; ++i) {
+              found = (extraInfos[i].studentId === extraInfo.studentId);
+            }
+            expect(found).toBe(false);
             done();
           });
         });
