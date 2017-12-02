0f25e2c3bab70698347ed5ef6b9dcd5b966f3b8e
kiddos <kiddo831007@gmail.com>
2017-12-02 23:15:37 +0800

    add region migration

--- migration.js
+++ migration.js
@@ -4,8 +4,10 @@ var admins = require('./models/admins');
 var students = require('./models/students');
 var teachers = require('./models/teachers');
 var classes = require('./models/classes');
+var regions = require('./models/regions');
 
 admins.migrate();
 students.migrate();
 teachers.migrate();
 classes.migrate();
+regions.migration();
