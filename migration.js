143cf33d98ae823effd0c7a4f06d02fa079125df
kiddos <kiddo831007@gmail.com>
2017-12-04 11:44:04 +0800

    add city migration

--- migration.js
+++ migration.js
@@ -4,10 +4,13 @@ var admins = require('./models/admins');
 var students = require('./models/students');
 var teachers = require('./models/teachers');
 var classes = require('./models/classes');
+var cities = require('./models/cities');
 var regions = require('./models/regions');
 
 admins.migrate();
 students.migrate();
 teachers.migrate();
 classes.migrate();
-regions.migration();
+cities.migration(function() {
+  regions.migration();
+});
