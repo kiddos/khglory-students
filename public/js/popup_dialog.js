3fbec4a8b99d95651f9c81af0069255ccb154dd0
kiddos <kiddo831007@gmail.com>
2017-12-06 12:33:40 +0800

    fix popup dialog

--- public/js/popup_dialog.js
+++ public/js/popup_dialog.js
@@ -1,15 +1,38 @@
+var popupMessageCallbacks = [];
+
 function popupMessage(title, message, yesCallback, noCallback) {
   $('#popup-title').text(title);
   $('#popup-text').text(message);
   $('#popup-dialog').fadeIn();
 
+  popupMessageCallbacks.push({
+    title: title,
+    message: message,
+    yes: yesCallback,
+    no: noCallback,
+  });
+
   $('#yes').on('click', function() {
     $('#popup-dialog').fadeOut(100);
-    if (yesCallback) yesCallback();
+    for (var i = 0; i < popupMessageCallbacks.length; ++i) {
+      if ($('#popup-title').text() === popupMessageCallbacks[i].title &&
+          $('#popup-text').text() === popupMessageCallbacks[i].message) {
+        yesCallback = popupMessageCallbacks[i].yes;
+        if (yesCallback) yesCallback();
+        break;
+      }
+    }
   });
 
   $('#no').on('click', function() {
     $('#popup-dialog').fadeOut();
-    if (noCallback) noCallback();
+    for (var i = 0; i < popupMessageCallbacks.length; ++i) {
+      if ($('#popup-title').text() === popupMessageCallbacks[i].title &&
+          $('#popup-text').text() === popupMessageCallbacks[i].message) {
+        noCallback = popupMessageCallbacks[i].no;
+        if (noCallback) noCallback();
+        break;
+      }
+    }
   });
 }
